7b5d08ecec220b63d03645e8b205703ef070d886
Huzaifa Nadeem <126342805+HuzaifaaNadeem@users.noreply.github.com>
2026-02-09 23:44:24 +0500

    Add Student Study Planner script

---
+++ LAB WORK/LAB 03/Q3.py
@@ -0,0 +1,9 @@
+print("Student Study Planner")
+print()
+
+subjects = ["AI", "Math", "Physics"]
+
+for subject in subjects:
+    print(f"Studying {subject}... Completed")
+
+print("Goal Achieved: All subjects completed")
